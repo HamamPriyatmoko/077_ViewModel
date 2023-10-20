dffb9085d894b6facc1f7b3008ce493bc18e3150
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-10-20 22:25:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gradel/Data/DataForm.kt
@@ -0,0 +1,3 @@
+package com.example.gradel.Data
+
+data class DataForm(val sex: String = "")
---
+++ app/src/main/java/com/example/gradel/Data/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.gradel.Data
+
+import com.example.gradel.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita,
+        R.string.wanita2,
+    )
+}
